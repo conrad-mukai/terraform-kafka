b65f3af07e2740c23dd13c3636b05d49149add59
Saurabh Jain <sjain@samsungnext.com>
2018-04-09 16:35:19 -0700

    Kafka Cluster WIP

--- data.tf
+++ data.tf
@@ -4,10 +4,10 @@
 
 data "aws_subnet" "subnet" {
   count = "${length(var.subnet_ids)}"
-  id = "${var.subnet_ids[count.index]}"
+  id    = "${var.subnet_ids[count.index]}"
 }
 
 data "aws_subnet" "static-subnet" {
   count = "${length(var.static_subnet_ids)}"
-  id = "${var.static_subnet_ids[count.index]}"
+  id    = "${var.static_subnet_ids[count.index]}"
 }
